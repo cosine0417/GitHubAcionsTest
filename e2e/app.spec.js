0f130cb9f2c950d2751483073c8f67135390d46d
Cherina <shen.yu.chan@gmail.com>
2025-07-01 21:52:57 +0800

    修正：更新測試中的 URL 以使用環境變數 BASE_URL

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -3,7 +3,8 @@ import { test, expect } from '@playwright/test';
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
+  // eslint-disable-next-line no-undef
+  await page.goto(process.env.BASE_URL || '/');
   // 初始顯示 0
   await expect(page.locator('span')).toHaveText('0')
 
