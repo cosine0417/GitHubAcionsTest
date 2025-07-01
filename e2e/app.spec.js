7db04f2f826f48d699335b66345048e18870aec6
Cherina <shen.yu.chan@gmail.com>
2025-07-01 21:51:21 +0800

    修正：將測試中的 URL 從完整地址更新為相對路徑

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
-  await page.goto('http://localhost:5173');
+  await page.goto('/');
   // 初始顯示 0
   await expect(page.locator('span')).toHaveText('0')
 
